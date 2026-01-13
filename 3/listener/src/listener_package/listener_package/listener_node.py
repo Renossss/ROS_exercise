594befa9c087aef6cfefe94e6af725496be2311e
RenosKomodromos <renoskomcha@gmail.com>
2026-01-13 12:55:19 +0200

    trdyr

--- 3/listener/src/listener_package/listener_package/listener_node.py
+++ 3/listener/src/listener_package/listener_package/listener_node.py
@@ -2,6 +2,9 @@
 import os
 import rclpy
 from rclpy.node import Node
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
 # Fill in something for msg type imports
 # from duckietown_msgs.msg import SOMETHING
@@ -11,8 +14,11 @@ class ListenerNode(Node):
     def __init__(self):
         super().__init__('listener_node')
         #Create publishers and subscribers in init, use callback
-        pass
-    
+
+        self.sub = self.create_subscription(String, 'chatter', self.callback_function, 10)  # create subscriber
+
+    def callback_function(self, msg):  # receive massage
+        self.get_logger().info('I heard: "%s"' % msg.data)  # parce it
     #Define callback functions here
 
 
